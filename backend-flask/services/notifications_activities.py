73f3f2269817999741b71aed16139e29728a99ab
Dhruva Sharma <invador0007@gmail.com>
2024-01-23 13:40:55 +0000

    incomplete integration of xray segments and sub segements to notifications activities.

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class NotificationsActivities:
   def run():
+    # xray ---
+    segment = xray_recorder.begin_segment('notifications_activities')
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
@@ -23,4 +27,11 @@ class NotificationsActivities:
       }],
     },
     ]
+    # xray ---
+    subsegment = xray_recorder.begin_subsegment('mock-data')
+    dict = {
+      "now": now.isoformat(),
+      "results-size": len(model['data'])
+    }
+    subsegment.put_metadata('key', dict, 'namespace')
     return results
